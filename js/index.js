709540de499873e79241774c98e968fbf70cf27d
wyclife omondi <wyliffeomondi01@gmail.com>
2021-06-22 00:59:51 +0300

    portfolio

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
-$(document).ready(function() {
-  $("h6").click(function() {
-    $(".design_icon.png").toggle();
-    $(".design_icon.png").toggle();
+$(document).ready(function(){
+  $(".img1").click(function(){
+    $(".design").toggle();
+    $(".text-click").toggle();
+  })
+
+  $(".img2").click(function(){
+    $(".dev").toggle();
+    $(".text-click2").toggle();
+  })
+
+  $(".img3").click(function(){
+    $(".product").toggle();
+    $(".text-click3").toggle();
+  })
+});
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideDown("slow");
   });
 });
+
+ 
