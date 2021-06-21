81155e9e22746fa095a61204e7640325f5831241
wyclife omondi <wyliffeomondi01@gmail.com>
2021-06-21 12:37:30 +0300

    footer

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
+ 
+$(document).ready(function(){
+  $(".img1").click(function(){
+    $(".design").toggle();
+    $(".text-click").toggle();
+  })
+
+  $(".img2").click(function(){
+    $(".dev").toggle();
+    $(".text-click2").toggle();
+  })
+
+  $(".img3").click(function(){
+    $(".product").toggle();
+    $(".text-click3").toggle();
+  })
+});
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideDown("slow");
+  });
+});
   
